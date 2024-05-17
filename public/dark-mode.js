2df908722a2f6029a7e192f85c9ace97bc8e81ad
claw1200 <carstenwood12@gmail.com>
2024-05-17 21:50:32 +0100

    Fixed css transition when loading page in dark mode

--- public/dark-mode.js
+++ public/dark-mode.js
@@ -1,68 +1,73 @@
-window.onload = function() {
+function darkModeToggle() {
+  //css dark mode
+  document.body.classList.toggle('dark-mode');
+  //invert button
+  var btn = document.getElementById('dark-mode-toggle');
+  btn.classList.toggle('inverted');
+  //invert logo img
+  img = document.getElementsByClassName("footer4-logo1");
+  for (i = 0; i < img.length; i++) {
+    img[i].classList.toggle('inverted');
+  }
+  // invert footer line
+  line = document.getElementsByClassName('thq-divider-horizontal');
+  for (i = 0; i < line.length; i++) {
+    //set bg color to white or black
+    line[i].style.backgroundColor = line[i].style.backgroundColor === 'rgb(255, 255, 255)' ? 'rgb(0, 0, 0)' : 'rgb(255, 255, 255)';
+  }
 
-    function darkModeToggle() {
-        //css dark mode
-        document.body.classList.toggle('dark-mode');
-        //invert button
-        var btn = document.getElementById('dark-mode-toggle');
-        btn.classList.toggle('inverted');
-        //invert logo img
-        img = document.getElementsByClassName("footer4-logo1");
-        for (i = 0; i < img.length; i++) {
-          img[i].classList.toggle('inverted');
-        }
-        // invert footer line
-        line = document.getElementsByClassName('thq-divider-horizontal');
-        for (i = 0; i < line.length; i++) {
-          //set bg color to white or black
-          line[i].style.backgroundColor = line[i].style.backgroundColor === 'rgb(255, 255, 255)' ? 'rgb(0, 0, 0)' : 'rgb(255, 255, 255)';
-        }
-      
-        //set img src to light.png or dark.png
-        var img = document.getElementById('dark-mode-toggle');
-        var src = img.getAttribute('src');
-        if (src == 'light.png') {
-          img.src = 'dark.png';
-          // set box shadow to black
-          for (let j = 2; j <= 7; j++) {
-            let box = document.getElementsByClassName(`features23-container${j}`);
-            for (let i = 0; i < box.length; i++) {
-              //set box shadow to black
-              box[i].style.boxShadow = 'rgba(0, 0, 0, 1) 2px 2px 4px 0px';
-            }
-          }
-          //save flag to local storage
-          localStorage.setItem('dark-mode', 'disabled');
-        } else {
-          img.src = 'light.png';
-          //set box shadow to white
-          for (let j = 2; j <= 7; j++) {
-            let box = document.getElementsByClassName(`features23-container${j}`);
-            for (let i = 0; i < box.length; i++) {
-              //set box shadow to white
-              box[i].style.boxShadow = 'rgba(255, 255, 255, 1) 2px 2px 4px 0px';
-            }
-          }
-          //save flag to local storage
-          localStorage.setItem('dark-mode', 'enabled');
-        } 
+  //set img src to light.png or dark.png
+  var img = document.getElementById('dark-mode-toggle');
+  var src = img.getAttribute('src');
+  if (src == 'light.png') {
+    img.src = 'dark.png';
+    // set box shadow to black
+    for (let j = 2; j <= 7; j++) {
+      let box = document.getElementsByClassName(`features23-container${j}`);
+      for (let i = 0; i < box.length; i++) {
+        //set box shadow to black
+        box[i].style.boxShadow = 'rgba(0, 0, 0, 1) 2px 2px 4px 0px';
       }
-      // check if dark mode is enabled
-      var darkModeEnabled = localStorage.getItem('dark-mode');
-      if (darkModeEnabled === 'enabled') {
-        darkModeToggle();
+    }
+    //save flag to local storage
+    localStorage.setItem('dark-mode', 'disabled');
+  } else {
+    img.src = 'light.png';
+    //set box shadow to white
+    for (let j = 2; j <= 7; j++) {
+      let box = document.getElementsByClassName(`features23-container${j}`);
+      for (let i = 0; i < box.length; i++) {
+        //set box shadow to white
+        box[i].style.boxShadow = 'rgba(255, 255, 255, 1) 2px 2px 4px 0px';
       }
-      else if (darkModeEnabled === 'disabled') {
-        // do nothing
-      }
-      else {
-        // default to dark mode
-        darkModeToggle();
-      }
-      
-      // add event listener to button
-      document.getElementById('dark-mode-toggle').addEventListener('click', function() {
-        darkModeToggle();
-      });
-  
+    }
+    //save flag to local storage
+    localStorage.setItem('dark-mode', 'enabled');
+  } 
+}
+
+
+window.onload = function() {  
+    // check if dark mode is enabled
+    var darkModeEnabled = localStorage.getItem('dark-mode');
+    if (darkModeEnabled === 'enabled') {
+      document.body.style.transition = 'none';
+      darkModeToggle();
+      setTimeout(function() {
+        document.body.style.transition = '';
+      }, 1000);
+    }
+    else if (darkModeEnabled === 'disabled') {
+      // do nothing
+    }
+    else {
+      // default to dark mode
+      // darkModeToggle();
+    }
+    
+    // add event listener to button
+    document.getElementById('dark-mode-toggle').addEventListener('click', function() {
+      darkModeToggle();
+    });
 };
+
--- public/index.js
+++ public/index.js
@@ -1,3 +1,52 @@
+function darkModeToggle() {
+  //css dark mode
+  document.body.classList.toggle('dark-mode');
+  //invert button
+  var btn = document.getElementById('dark-mode-toggle');
+  btn.classList.toggle('inverted');
+  //invert logo img
+  img = document.getElementsByClassName("footer4-logo1");
+  for (i = 0; i < img.length; i++) {
+    img[i].classList.toggle('inverted');
+  }
+  // invert footer line
+  line = document.getElementsByClassName('thq-divider-horizontal');
+  for (i = 0; i < line.length; i++) {
+    //set bg color to white or black
+    line[i].style.backgroundColor = line[i].style.backgroundColor === 'rgb(255, 255, 255)' ? 'rgb(0, 0, 0)' : 'rgb(255, 255, 255)';
+  }
+
+  //set img src to light.png or dark.png
+  var img = document.getElementById('dark-mode-toggle');
+  var src = img.getAttribute('src');
+  if (src == 'light.png') {
+    img.src = 'dark.png';
+    // set box shadow to black
+    for (let j = 2; j <= 7; j++) {
+      let box = document.getElementsByClassName(`features23-container${j}`);
+      for (let i = 0; i < box.length; i++) {
+        //set box shadow to black
+        box[i].style.boxShadow = 'rgba(0, 0, 0, 1) 2px 2px 4px 0px';
+      }
+    }
+    //save flag to local storage
+    localStorage.setItem('dark-mode', 'disabled');
+  } else {
+    img.src = 'light.png';
+    //set box shadow to white
+    for (let j = 2; j <= 7; j++) {
+      let box = document.getElementsByClassName(`features23-container${j}`);
+      for (let i = 0; i < box.length; i++) {
+        //set box shadow to white
+        box[i].style.boxShadow = 'rgba(255, 255, 255, 1) 2px 2px 4px 0px';
+      }
+    }
+    //save flag to local storage
+    localStorage.setItem('dark-mode', 'enabled');
+  } 
+}
+
+
 var TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
@@ -40,9 +89,9 @@ TxtType.prototype.tick = function() {
 };
 
 window.onload = function() {
+  
+
   // begin particle script
-
-
   var WIDTH;
   var HEIGHT;
   var canvas;
@@ -150,71 +199,28 @@ window.onload = function() {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 
-    function darkModeToggle() {
-      //css dark mode
-      document.body.classList.toggle('dark-mode');
-      //invert button
-      var btn = document.getElementById('dark-mode-toggle');
-      btn.classList.toggle('inverted');
-      //invert logo img
-      img = document.getElementsByClassName("footer4-logo1");
-      for (i = 0; i < img.length; i++) {
-        img[i].classList.toggle('inverted');
-      }
-      // invert footer line
-      line = document.getElementsByClassName('thq-divider-horizontal');
-      for (i = 0; i < line.length; i++) {
-        //set bg color to white or black
-        line[i].style.backgroundColor = line[i].style.backgroundColor === 'rgb(255, 255, 255)' ? 'rgb(0, 0, 0)' : 'rgb(255, 255, 255)';
-      }
-    
-      //set img src to light.png or dark.png
-      var img = document.getElementById('dark-mode-toggle');
-      var src = img.getAttribute('src');
-      if (src == 'light.png') {
-        img.src = 'dark.png';
-        // set box shadow to black
-        for (let j = 2; j <= 7; j++) {
-          let box = document.getElementsByClassName(`features23-container${j}`);
-          for (let i = 0; i < box.length; i++) {
-            //set box shadow to black
-            box[i].style.boxShadow = 'rgba(0, 0, 0, 1) 2px 2px 4px 0px';
-          }
-        }
-        //save flag to local storage
-        localStorage.setItem('dark-mode', 'disabled');
-      } else {
-        img.src = 'light.png';
-        //set box shadow to white
-        for (let j = 2; j <= 7; j++) {
-          let box = document.getElementsByClassName(`features23-container${j}`);
-          for (let i = 0; i < box.length; i++) {
-            //set box shadow to white
-            box[i].style.boxShadow = 'rgba(255, 255, 255, 1) 2px 2px 4px 0px';
-          }
-        }
-        //save flag to local storage
-        localStorage.setItem('dark-mode', 'enabled');
-      } 
-    }
+
     // check if dark mode is enabled
     var darkModeEnabled = localStorage.getItem('dark-mode');
     if (darkModeEnabled === 'enabled') {
+      document.body.style.transition = 'none';
       darkModeToggle();
+      setTimeout(function() {
+        document.body.style.transition = '';
+      }, 1000);
     }
     else if (darkModeEnabled === 'disabled') {
       // do nothing
     }
     else {
       // default to dark mode
-      darkModeToggle();
+      // darkModeToggle();
     }
     
     // add event listener to button
     document.getElementById('dark-mode-toggle').addEventListener('click', function() {
       darkModeToggle();
     });
-
   };
 
 // call request api to get stats
